2e8ab3542aed7874a3fee9c1a16a6321840e255d
Maciej Anioł <maciej.aniol.programista@gmail.com>
2020-11-01 18:05:46 +0100

    Player paddle control added

--- paddle.cpp
+++ paddle.cpp
@@ -1,6 +1,35 @@
 #include "paddle.h"
+#include <QKeyEvent>
+#include <QGraphicsScene>
+#include <ball.h>
 
-Paddle::Paddle()
+Paddle::Paddle(int width, int height) : HEIGHT(height), WIDTH(width)
 {
-
+   setRect(0, 0, WIDTH, HEIGHT);
 }
+
+int Paddle::getWidth() const
+{
+    return WIDTH;
+}
+
+int Paddle::getHeight() const
+{
+    return HEIGHT;
+}
+
+
+void Paddle::keyPressEvent(QKeyEvent *event)
+{
+   if (event->key() == Qt::Key_Up)
+   {
+       if(y()>0)
+       setPos(x(),y()-10);
+   }
+   else if (event->key() == Qt::Key_Down)
+   {
+       if(y()<500)
+       setPos(x(),y()+10);
+    }
+}
+
